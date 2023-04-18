9b515d650d31147076679570a620a8a5d95d3e78
nnda <96333146+nndda@users.noreply.github.com>
2023-04-18 17:49:34 +0800

    Update LevelHandler.gd

--- Scripts/LevelHandler.gd
+++ Scripts/LevelHandler.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
-@export_node_path( "ColorRect" ) var camera_boundary
+@export_node_path( "ColorRect", "ReferenceRect" ) var camera_boundary
+@export_category( "Objectives" )
+@export var gearwheel_objectives : Array[ NodePath ]
+@export var spider_objectives : Array[ NodePath ]
+@export_node_path( "Node2D" ) var door_to_next_level
+
+signal objective_completed
 
 func _ready():
 	var camlim = get_node( camera_boundary )
@@ -10,3 +16,13 @@ func _ready():
 		int( camlim.position.x + camlim.size.x ),
 		int( camlim.position.y + camlim.size.y )
 	]
+
+	for gear in gearwheel_objectives:
+		get_node( gear ).connect( "activated", Callable( self, "ObjectiveCompleted" ) )
+
+
+func  ObjectiveCompleted() -> Array[ bool ]:
+	var check_gears : Array[ bool ] = []
+	for gear in gearwheel_objectives:
+		check_gears.append( get_node( gear ).active )
+	return check_gears
