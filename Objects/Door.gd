95e6e148866f679a2cf9378aa059d418956e1316
nnda <96333146+nndda@users.noreply.github.com>
2023-04-18 17:46:32 +0800

    Create door for level transition

---
+++ Objects/Door.gd
@@ -0,0 +1,13 @@
+extends Sprite2D
+
+func ToNextLevel() -> void:
+
+	var next_level : String = "res://Levels/" +\
+	str( int( glbl.current_level.get_name().right( 2 ) ) + 1 ) + ".tscn"
+
+	get_tree().change_scene_to_file( next_level )
+
+func CompleteObjective() -> void: pass
+
+func _on_interracted():
+	pass # Replace with function body.
