e9d151b0fa21f2c733a500ad2b291d4f6cb5188b
nnda <96333146+nndda@users.noreply.github.com>
2023-04-18 17:45:42 +0800

    Clean up

--- Objects/Gearwheel.gd
+++ Objects/Gearwheel.gd
@@ -3,17 +3,17 @@ extends Node2D
 var running : bool = false
 
 @export var auto_run : bool = false
-@export_enum("Running","Ticking") var movement_type = 0
-@export_range(15,360) var ticking_speed = 15
-@export_range(1,30) var running_speed = 25
+@export_enum( "Running", "Ticking" ) var movement_type = 0
+@export_range( 15, 360 ) var ticking_speed : float = 15
+@export_range( 1, 30 ) var running_speed : float = 25
+
+@export var callback : String
 
 @export_category( "Gearwheel Objects" )
 @export var gearwheels : Array[ NodePath ]
 @export var chains : Array[ NodePath ]
 @export var chains_speed : PackedFloat32Array
-@export_node_path("Node2D") var sub_gearwheel : NodePath
-#@export var has_chain : bool = false
-#@export_range(1.0,20.0) var chain_speed : float = 2.0
+@export_node_path( "Node2D" ) var sub_gearwheel : NodePath
 
 @export_category( "Switches" )
 @export var cobwebs : Array[ NodePath ]
@@ -21,6 +21,9 @@ var running : bool = false
 @export var switch_nodes : Array[ NodePath ]
 var cobwebs_objects : Array[ Node ]
 
+
+signal activated
+var active : bool = false
 
 
 var ticking_tween : Tween
@@ -32,12 +35,8 @@ func TickingAnim() -> void:
 
 	for gears in gearwheels:
 		var rotate_dir : float = ticking_speed if get_node( gears ).get_name().ends_with( "+" ) else -ticking_speed
-#		if get_node( gears ).get_name().ends_with( "+" ): rotate_dir = ticking_speed
-#		else: rotate_dir = -ticking_speed
-#		rotate_dir *= ticking_speed
 
-		ticking_tween.tween_property(get_node( gears ), "rotation_degrees", -rotate_dir, 1).as_relative()
-		ticking_tween.tween_property(get_node( gears ), "rotation_degrees", rotate_dir, 1).as_relative()
+		ticking_tween.tween_property( get_node( gears ), "rotation_degrees", rotate_dir, 1 ).as_relative()
 
 	ticking_tween.play()
 
@@ -77,22 +76,19 @@ func RunAuto() -> void:
 	if get_node_or_null( sub_gearwheel ) != null:
 		get_node_or_null( sub_gearwheel ).RunAuto()
 
+	active = true
+	emit_signal( "activated" )
+
 func Run() -> void:
 	if cobwebs_objects.size() == 0:
 		RunAuto()
 
 func _on_child_exiting_tree( node ):
-#	Run()
 	if cobwebs_objects.has( node ):
 		cobwebs_objects.erase( node )
-	Run()
 
+	if cobwebs_objects.is_empty() and !active: Run()
 
-
-
-
-
-
-
-
-
+func _on_tree_exiting():
+	if running_tween != null: running_tween.kill()
+	if ticking_tween != null: ticking_tween.kill()
