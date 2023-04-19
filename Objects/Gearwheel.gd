7443097311945567298a8346051110753f95b616
nnda <96333146+nndda@users.noreply.github.com>
2023-04-19 15:15:04 +0800

    Clean up

--- Objects/Gearwheel.gd
+++ Objects/Gearwheel.gd
@@ -7,7 +7,9 @@ var running : bool = false
 @export_range( 15, 360 ) var ticking_speed : float = 15
 @export_range( 1, 30 ) var running_speed : float = 25
 
-@export var callback : String
+@export_subgroup( "Callback" )
+@export_node_path( "Node" ) var callback_at : NodePath
+@export var callback_function : String # can be a function or animation name
 
 @export_category( "Gearwheel Objects" )
 @export var gearwheels : Array[ NodePath ]
@@ -34,9 +36,13 @@ func TickingAnim() -> void:
 	set_trans( Tween.TRANS_BOUNCE )
 
 	for gears in gearwheels:
-		var rotate_dir : float = ticking_speed if get_node( gears ).get_name().ends_with( "+" ) else -ticking_speed
+		var rotate_dir : float =\
+		ticking_speed if get_node( gears ).get_name().ends_with( "+" )\
+		else -ticking_speed
 
-		ticking_tween.tween_property( get_node( gears ), "rotation_degrees", rotate_dir, 1 ).as_relative()
+		ticking_tween.tween_property(
+			get_node( gears ), "rotation_degrees",
+			rotate_dir, 1 ).as_relative()
 
 	ticking_tween.play()
 
@@ -46,13 +52,16 @@ func RunningAnim() -> void:
 	running_tween = create_tween().set_loops().set_parallel()
 
 	for gears in gearwheels:
-		var rotate_dir : float = 360 if get_node( gears ).get_name().ends_with( "+" ) else -360
+		var rotate_dir : float =\
+		360 if get_node( gears ).get_name().ends_with( "+" )\
+		else -360
 
 		running_tween.tween_property(
 			get_node( gears ), "rotation_degrees",
 			rotate_dir,
 			remap( running_speed, 1, 30, 30, 1 )
 		).from( 0 )
+
 	running_tween.play()
 
 
@@ -92,3 +101,6 @@ func _on_child_exiting_tree( node ):
 func _on_tree_exiting():
 	if running_tween != null: running_tween.kill()
 	if ticking_tween != null: ticking_tween.kill()
+
+#func _on_screen_entered(): show()
+#func _on_screen_exited(): hide()
