db545cec169d28ccb703d2f3126f7c19dac445cd
nnda <96333146+nndda@users.noreply.github.com>
2023-04-20 20:50:44 +0800

    Add option for custom animation

--- Objects/Gate.gd
+++ Objects/Gate.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@export_node_path( "AnimationPlayer" ) var animation_node : NodePath
 @export var animation_name : String = "OpenDefault"
 @export var conditional : int = 99
 var ot : bool = true
@@ -7,7 +8,8 @@ var ot : bool = true
 func Open() -> void:
 	if ot:
 		$Gearwheel.RunAuto()
-		$AnimationPlayer.play( animation_name )
+#		$AnimationPlayer.play( animation_name )
+		get_node( animation_node ).play( animation_name )
 		$TileMap.process_mode = Node.PROCESS_MODE_DISABLED
 		ot = false
 
