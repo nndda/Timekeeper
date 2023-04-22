176bd20f9e692d931e21349dfffdd0eb31b99bcc
nnda <96333146+nndda@users.noreply.github.com>
2023-04-22 19:11:39 +0800

    Add get_children_if()

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -13,3 +13,11 @@ var camera_boundary : PackedInt32Array = [0,0,0,0]
 
 var current_level : Object
 var current_interractable : Object
+
+
+func get_children_if( prefix : String, target : Node ) -> Array[ Node ]:
+	var output : Array[ Node ] = []
+	for child in target.get_children():
+		if child.get_name().begins_with( prefix ):
+			output.append( child )
+	return output
