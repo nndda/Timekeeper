a1128e5bdd207ad9d6d65ad7e39c509da0b87cf3
nnda <96333146+nndda@users.noreply.github.com>
2023-04-22 19:07:14 +0800

    Set default animation

--- Objects/Gate.gd
+++ Objects/Gate.gd
@@ -1,17 +1,15 @@
 extends Node2D
 
 @export_node_path( "AnimationPlayer" ) var animation_node : NodePath
-@export var animation_name : String = "OpenDefault"
+var animation_name : String = "OpenDefault"
 @export var conditional : int = 99
 var ot : bool = true
 
-func Open() -> void:
-	if ot:
-		$Gearwheel.RunAuto()
-#		$AnimationPlayer.play( animation_name )
-		get_node( animation_node ).play( animation_name )
-		$TileMap.process_mode = Node.PROCESS_MODE_DISABLED
-		ot = false
+func Open() -> void: if ot:
+	$Gearwheel.RunAuto()
+	get_node( animation_node ).play( animation_name )
+	$TileMap.process_mode = Node.PROCESS_MODE_DISABLED
+	ot = false
 
 func OpenSignal( conditional_count ) -> void:
 	if conditional_count >= conditional: Open()
